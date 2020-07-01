a54be006080379037c51228cf4057162b904de93
Christy Hauschild Schott <christyhauschild@gmail.com>
2020-07-01 07:58:54 -0300

    create network body

--- src/pages/Network/index.tsx
+++ src/pages/Network/index.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import Publish from 'components/Publish';
+import Ad from 'components/Ad';
+import {
+    WrapperAd, Container, Main, Aside,
+} from './styles';
 
-const Network: React.FC = () => <div />;
+const Network: React.FC = () => (
+    <WrapperAd>
+        <Ad />
+        <Container>
+            <Aside>
+                <Publish banner />
+            </Aside>
+            <Main>
+                Oi
+            </Main>
+        </Container>
+    </WrapperAd>
+);
 
 export default Network;
